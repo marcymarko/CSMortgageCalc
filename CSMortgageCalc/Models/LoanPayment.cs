3252580fdc6cb6d6eefaabbcc9d83f16a256086b
marcymarko <82332440+marcymarko@users.noreply.github.com>
2022-03-01 23:56:41 -0500

    Initial commit

---
+++ CSMortgageCalc/Models/LoanPayment.cs
@@ -0,0 +1,12 @@
+﻿namespace CSMortgageCalc.Models
+{
+    public class LoanPayment
+    {
+        public int Month { get; set; }
+        public decimal Payments { get; set; }
+        public decimal MonthlyPrincipal { get; set; }
+        public decimal MonthlyInterest { get; set; }
+        public decimal TotalInterest { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
